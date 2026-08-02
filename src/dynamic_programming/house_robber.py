442248d5df1123b40d1fd9ab63b1440bb3f544a7
cd155 <cd155@163.com>
2026-08-02 13:04:37 -0400

    Refactor max_money_index_of function for clarity and efficiency

--- src/dynamic_programming/house_robber.py
+++ src/dynamic_programming/house_robber.py
@@ -44,24 +44,17 @@ class Solution:
             if n in max_money_index_hash:
                 return max_money_index_hash[n]
             
-            max_money_1 = 0
-            max_money_2 = 0
-
             if n >= size: 
                 return 0
-            elif n+1<size and nums[n+1]>nums[n]:
-                max_money_1 = nums[n+1] + max_money_index_of(n+3)
 
-            max_money_2 = nums[n] + max_money_index_of(n+2)
-            result = max(max_money_1, max_money_2)
+            rob_house = nums[n] + max_money_index_of(n+2)
+            skip_house =  max_money_index_of(n+1)
+
+            result = max(rob_house, skip_house)
             max_money_index_hash[n] = result
             return result
 
-        max_money = 0    
-        for i in range(0, len(nums)):
-            max_money = max(max_money, max_money_index_of(i))
-
-        return max_money
+        return max_money_index_of(0)
 
 
 # Example usage (for testing locally)
