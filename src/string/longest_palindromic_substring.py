8a6cb0445b5650bd0440addb656f158e5c289835
cd155 <cd155@163.com>
2026-08-04 22:15:51 -0400

    Implement longestPalindrome method to find the longest palindromic substring

--- src/string/longest_palindromic_substring.py
+++ src/string/longest_palindromic_substring.py
@@ -32,9 +32,27 @@ class Solution:
         Time Complexity: O(n^2)
         Space Complexity: O(1)
         """
-        # TODO: Implement solution
-        pass
+        size = len(s)
 
+        def find_palindromic_str(left, right):
+            while (left >= 0 and right < size and s[left] == s[right]):
+                left -= 1
+                right += 1
+            return left+1, right-1
+
+        result  = 0, 0
+        for i in range(0, size):
+            # examine palindrome in odd case
+            left, right = find_palindromic_str(i-1, i+1)
+            if(right-left) > (result[1] - result[0]):
+                result = left, right
+
+            # examine palindrome in even case
+            left, right = find_palindromic_str(i, i+1)
+            if(right-left) > (result[1] - result[0]):
+                result = left, right
+
+        return s[result[0]:(result[1]+1)]
 
 # Example usage (for testing locally)
 if __name__ == "__main__":
