66d2fc754c6e5297247209014fe1b0c2c6fa4d6f
cd155 <cd155@163.com>
2026-09-23 12:44:32 -0400

    Implement DFS for word search and optimize trie insertion in findWords method

--- src/tries/word_search_ii.py
+++ src/tries/word_search_ii.py
@@ -44,14 +44,41 @@ class Solution:
         Time Complexity: O(m * n * 4^L) where L is max word length
         Space Complexity: O(k) where k is total characters in words
         """
-        all_possible_words = self.find_all_words(board)
-        for p_word in all_possible_words:
-            self.trie.insert(p_word)
+        for word in words:
+            self.trie.insert(word)
+
+        words = set(words)
 
         result = []
-        for f_w in words:
-            if self.trie.startsWith(f_w):
-                result.append(f_w)
+        m = len(board)
+        n = len(board[0])
+
+        def dfs(i, j, visited, path, node):
+            visited.add((i,j))
+            path.append(board[i][j])
+
+            current_word = "".join(path)
+            if current_word in words and current_word not in result:
+                result.append(current_word)
+
+            ds = [(1,0), (0,1), (-1,0), (0,-1)]
+            for d_i, d_j in ds:
+                next_i, next_j = i+d_i, j+d_j
+                if 0<=next_i<m and 0<=next_j<n and (next_i,next_j) not in visited:
+                    if board[next_i][next_j] in node.children:
+                        next_node = node.children[board[next_i][next_j]]
+                        dfs(next_i, next_j, visited, path, next_node)
+
+                        if not next_node.children:
+                            node.children.pop(board[next_i][next_j], None)
+            path.pop()
+            visited.remove((i,j))
+        
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] in self.trie.root.children:
+                    dfs(i, j, set(), [], self.trie.root.children[board[i][j]])
+
         return result
 
     def find_all_words(self, board):
@@ -97,6 +124,6 @@ if __name__ == "__main__":
 
     # Test case 2
     board = [["a", "b"], ["c", "d"]]
-    words = ["abcb"]
+    words = ["abdb"]
     result = solution.findWords(board, words)
     print(f"Test 2: {result}")
